6163b795b2b633e2b77cb337263f5aa6eba72c21
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-08-08 19:04:05 +0200

    Added my name as AssemblyCompany

--- source/SamoSsas.Core/Properties/AssemblyInfo.cs
+++ source/SamoSsas.Core/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SamoSsas.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Cédric L. Charlier")]
 [assembly: AssemblyProduct("SamoSsas.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
